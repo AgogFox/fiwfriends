a17b4bb0b03b56e7f0d09ed799f2e5d8cf10a8ad
AgogFox <bopawat@hotmail.com>
2024-03-05 22:09:46 +0700

    initial commit

---
+++ FiwFriends/Controllers/EventController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Test.Models.Event;
+
+namespace Test.Controllers
+{
+    public class EventController : Controller
+    {
+        private string file = @"D:\Webapp\Test\Data\EventDB.json";
+
+        private readonly ILogger<EventController> _logger;
+
+        public EventController(ILogger<EventController> logger)
+        {
+            _logger = logger;
+        }
+
+        private List<EventOBJ> GetEvents(string filepath)
+        {
+            if (System.IO.File.Exists(filepath))
+            {
+                var json = System.IO.File.ReadAllText(filepath);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<List<EventOBJ>>(json);
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+            }
+            return null;
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string word)
+        {
+            if (word == null)
+            {
+                return View();
+            }
+            var eventdb = GetEvents(file);
+            var list = new List<EventOBJ>();
+            foreach (var e in eventdb)
+            {
+                if (e.location.Contains(word) == true)
+                {
+                    list.Add(e);
+                }
+            }
+            if (list.Count() == 0)
+            {
+
+                ViewBag.errormsg = "Can not found";
+                return View();
+            }
+            ViewBag.word = word;
+            return View(list);
+        }
+    }
+}
--- FiwFriends/Controllers/HomeController.cs
+++ FiwFriends/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-using FiwFriends.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Test.Models;
+using static Test.Models.Event;
 
-namespace FiwFriends.Controllers
+namespace Test.Controllers
 {
     public class HomeController : Controller
     {
@@ -13,10 +14,40 @@ namespace FiwFriends.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() {
+            return View(); 
+        
+        }
+
+        /*public IActionResult Search()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Search(string word)
+        {
+            if (word == null) {
+                return View();
+            }
+            var eventdb = GetEvents(file);
+            var list = new List<EventOBJ>();
+            foreach (var e in eventdb)
+            {
+                if (e.location.Contains(word) == true)
+                {
+                    list.Add(e);
+                }
+            }
+            if (list.Count() == 0) {
+
+                    ViewBag.errormsg = "Can not found";
+                    return View();
+            
+            }
+            return View(list);
+        }
+
 
         public IActionResult Privacy()
         {
@@ -27,6 +58,6 @@ namespace FiwFriends.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }*/
     }
 }
