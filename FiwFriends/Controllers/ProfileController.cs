efd5c6dca164522294d7361b517ea36423d64bad
AgogFox <bopawat@gmail.com>
2024-03-05 22:34:10 +0700

    chore: rebase to new dev (after dereferenced)

---
+++ FiwFriends/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using NuGet.DependencyResolver;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            var jsonData = System.IO.File.ReadAllText(@"C:\Users\Mon\Desktop\WebApplication1\WebApplication1\Database\UserDB.json");
+            var userlist = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Profile>>(jsonData);
+            return View(userlist);
+            
+        }
+    }
+}
